d24f02cd28f243d4f9e011a86cf3ba28d34f44eb
Rishabh Nandwal <nanris01@CSGICORP.COM>
2021-05-10 16:23:02 +0530

    HelloWorld

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+//using namespace std;
+
+int main(){
+    int a=56;
+    //std::cout<<"Enter a number";
+    //std::cin>>a;
+    std::cout<<"Hello there!! Value of a: "<< a;
+    return 0;
+}
